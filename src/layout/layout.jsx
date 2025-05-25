9e82f9c3e4968c3bb717bb4de3abab6de6239782
MAPRITOM <pritom746@gmail.com>
2025-05-25 19:08:58 +0600

    Mern_001

---
+++ src/layout/layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppNav from "../component/app-nav.jsx";
+import Footer from "../component/footer.jsx";
+const Layout = (props) => {
+
+    return (
+        <>
+            <AppNav/>
+                {props.children}
+            <Footer/>
+        </>
+    );
+};
+
+export default Layout;
